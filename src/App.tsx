360bfd7c927e6f0751ef3e9208c9e93f344d81d0
Khusamov Sukhrob <khusamov@yandex.ru>
2019-12-15 16:33:18 +0300

    Добавлены примеры вывода версии и даты сборки

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,8 @@ export default class App extends Component {
 	render() {
 		return (
 			<div className='App'>
-				App
+				<div>Версия: {process.env.BUILD_VERSION}</div>
+				<div>Дата сборки: {new Date(process.env.BUILD_DATE!).toString()}</div>
 			</div>
 		);
 	}
